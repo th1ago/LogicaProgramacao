287363f30bbf309b398e17bd79f2b41fb30bde4d
Thiago <thiago_murakami@hotmail.com>
2023-04-17 18:29:45 -0300

    add new files to new project

---
+++ index.js
@@ -0,0 +1,10 @@
+function mostra(frase){
+    document.write(frase + "<br>")
+}
+
+function TabuadaWhile(){
+    var multiplicado = 1;
+    while(multiplicado <= 10){
+        mostra(7 * multiplicado);
+    }
+}
