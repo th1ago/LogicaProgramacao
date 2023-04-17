c785d95719269dd3f996627cf63f0c95fcd7177d
Thiago <thiago_murakami@hotmail.com>
2023-04-17 18:48:34 -0300

    add loops

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-function mostra(frase){
-    document.write(frase + "<br>")
-}
-
 function TabuadaWhile(){
     var multiplicado = 1;
     while(multiplicado <= 10){
-        mostra(7 * multiplicado);
+        console.log("tabuada do 7 " + 7 * multiplicado);
+        multiplicado++;
     }
 }
+
+function TabuadaFor(){
+    var numeroTexto = prompt("Qual a tabuada");
+    var numeroTabuada = parseInt(numeroTexto);
+
+    for(mult = 1; mult <=10; mult++){
+        console.log("tabuada do 5" + numeroTabuada * mult);
+    }
+}
+
+TabuadaFor();
