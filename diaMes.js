d61cc0d75d217a542a0326fca8fde4a0f38622a7
Thiago <thiago_murakami@hotmail.com>
2023-04-27 16:02:12 -0300

    apply forEach

--- diaMes.js
+++ diaMes.js
@@ -3,7 +3,12 @@ function calcularDiasFaltantes(){
     var mes = parseInt(prompt("Digite uma data"))
 
     var mes28 = 2;
+    var meses30 = [4, 6, 9, 11]
     
+    meses30.forEach(function(mes30){
+        return mes30;
+    })
+
     if (mes == mes28){
         faltante = 28 - dia;      
     } else {
