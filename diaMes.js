f50327f5ea14ea23ae729cde2c60c27a5e73d385
Thiago <thiago_murakami@hotmail.com>
2023-04-28 19:59:08 -0300

    create logic

--- diaMes.js
+++ diaMes.js
@@ -5,14 +5,14 @@ function calcularDiasFaltantes(){
     var mes28 = 2;
     var meses30 = [4, 6, 9, 11]
     
-    meses30.forEach(function(mes30){
-        return mes30;
-    })
-
-    if (mes == mes28){
-        faltante = 28 - dia;      
-    } else {
-        faltante = 31 - dia
+    for(i = 0; i < meses30.length; i++ ){
+        if (mes == mes28){
+            faltante = 28 - dia;      
+        } else if (mes == meses30){
+            faltante = 30 - dia
+        } else {
+            faltante = 31 - dia
+        }
     }
 
     console.log("Faltam", faltante, "no", mes)
